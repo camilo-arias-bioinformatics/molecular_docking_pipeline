5251fe62cf3cbc0b2387837536384f5cfef03120
Camilo Arias <j.camilo.ariasospina@gmail.com>
2026-03-12 07:10:23 -0500

    Update prepare_compound_library_for_docking.sh

--- scripts/prepare_compound_library_for_docking.sh
+++ scripts/prepare_compound_library_for_docking.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Create  directory to store the pdbqt files for the compound library
+mkdir compound_library_fragmented
+mkdir compoun_library
+
+# Extract individual sdf files from compound library search sdf file
+obabel compound_library.sdf -O compound_library_fragmented/ligand_.sdf -m
+
+for f in compound_library_fragmented/*.sdf; do
+    base=$(basename "$f")
+    obabel "$f" -O "compound_library/$base" --largest -h
+done
