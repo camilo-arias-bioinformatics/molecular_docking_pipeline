0d4fdaae8ee3a96f10e995e1fe029ff77f980224
Camilo Arias <j.camilo.ariasospina@gmail.com>
2026-03-11 22:14:08 -0500

    Update configure_grid_box.sh

--- scripts/configure_grid_box.sh
+++ scripts/configure_grid_box.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 
+# Create a grid box with a standard distance of 5 A, distance should be adjusted for different projects
+mk_prepare_receptor.py -i protein.pdb -o grid -v --box_enveloping ligand.pdb --padding=5
