30c6e4c25e5dc5f0457dd91594215c82fa98d622
Camilo Arias <j.camilo.ariasospina@gmail.com>
2026-03-12 08:46:57 -0500

    Update prepare_compound_library_for_docking.sh

--- scripts/prepare_compound_library_for_docking.sh
+++ scripts/prepare_compound_library_for_docking.sh
@@ -2,7 +2,8 @@
 
 # Create  directory to store the pdbqt files for the compound library
 mkdir compound_library_fragmented
-mkdir compoun_library
+mkdir compoun_library_sdf
+mkdir compoun_library_pdbqt
 
 # Extract individual sdf files from compound library search sdf file
 obabel compound_library.sdf -O compound_library_fragmented/ligand_.sdf -m
@@ -10,4 +11,9 @@ obabel compound_library.sdf -O compound_library_fragmented/ligand_.sdf -m
 for f in compound_library_fragmented/*.sdf; do
     base=$(basename "$f")
     obabel "$f" -O "compound_library/$base" --largest -h
+done
+
+for f in compound_library/*.sdf; do
+    base=$(basename "$f" .sdf)
+    mk_prepare_ligand.py -i "$f" -o "compound_library_pdbqt/$base.pdbqt"
 done
